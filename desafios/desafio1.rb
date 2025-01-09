fa469878ebf8fbb6248ed8c74a4a6640643e655d
Diogo <diogo@Note-Home.bbrouter>
2025-01-09 12:18:52 -0300

    desafio

---
+++ desafios/desafio1.rb
@@ -0,0 +1,10 @@
+print "Digite o seu Nome :"
+nome = gets.chomp
+
+print "Digite seu Sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp.to_i
+
+puts "Olá! #{nome} #{sobrenome}, de #{idade} anos, seu cadastro foi realizado!"
