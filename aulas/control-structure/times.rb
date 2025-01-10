290078350027f23d770a8f08c52a5539a137cf56
Diogo <diogo@Note-Home.bbrouter>
2025-01-10 11:14:29 -0300

    estruturas de repetições

---
+++ aulas/control-structure/times.rb
@@ -0,0 +1,15 @@
+#exemplo de print
+6.times {print "hi "}
+
+#exemplo de puts
+6.times {puts "hi"}
+
+puts "---"
+
+4.times do
+    puts "hi"
+end
+
+10.times do |contador|
+    puts "#{contador}"
+end 
